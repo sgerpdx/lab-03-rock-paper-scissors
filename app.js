32ae3ba4a71921f32d0fdf1d16b620649bb646f8
Dominique Sakrisson <domsakrisson@gmailcom>
2021-01-13 16:52:18 -0800

    FIX ERROR WITH OUTPUT FOR LOSES

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const resetButton = document.getElementById('reset-button');
 const winsDisplay = document.getElementById('times-won');
 const totalsDisplay = document.getElementById('times-played');
 const tiesDisplay = document.getElementById('times-draw');
+const lossDisplay = document.getElementById('times-lost');
 
 
 // initialize state
@@ -13,7 +14,8 @@ const tiesDisplay = document.getElementById('times-draw');
 const defaultStrings = [
     'Wins will show here', 
     'Total plays will show here', 
-    'Times tied will show here'];
+    'Times tied will show here', 
+    'Times lost displays here'];
 
 //declare some string values 
 const paper = 'paper';
@@ -23,6 +25,7 @@ const scissors = 'scissors';
 let totalPlays = 0;
 let totalWins = 0;
 let totalTies = 0;
+let totalLosses = 0;
 
 let userWin = false;
 let userDraw = false;
@@ -48,16 +51,7 @@ function testIfWin(userGuess, compChoice){
     } else {
         return 'lose';
     }
-    /*
-    if (userWin){
-        totalWins++;
-        userWin = false;
-    }
-    if (userDraw){
-        totalTies++;
-        userDraw = false;
-    }
-    */
+   
 }
 
  
@@ -71,30 +65,35 @@ playButton.addEventListener('click', () => {
     //set comps choice to the random number returned from getRandThrow()
     let compChoice = convertCompNumToValue(getRandThrow());
     //call to test if user guess was correct
+    
 
     //by default the user will guess the incorrect answer
     if (testIfWin(userGuess, compChoice) === 'win') {
         totalWins++;
     } else if (testIfWin(userGuess, compChoice) === 'draw'){
         totalTies++;
-    } else{
-        
+    } else {
+        totalLosses++;
     }
     //declare the strings for the wins, total, and draws
     let winString = `You won: ${totalWins} times!`;
     let totalString = `You played ${totalPlays} times!`;
     let drawString = `You tied ${totalTies} times!`;
+    let lossString = `You tied ${totalLosses} times!`;
     //display the strings for wins, total, and draws
     winsDisplay.textContent = winString;
     totalsDisplay.textContent = totalString;
     tiesDisplay.textContent = drawString; 
+    lossDisplay.textContent = lossString;
 });
 
 resetButton.addEventListener('click', () => {
     totalWins = 0;
     totalPlays = 0;
     totalTies = 0;
+    totalLosses = 0;
     winsDisplay.textContent = defaultStrings[0];
     totalsDisplay.textContent = defaultStrings[1];
     tiesDisplay.textContent = defaultStrings[2]; 
+    lossDisplay.textContent = defaultStrings[3];
 });
